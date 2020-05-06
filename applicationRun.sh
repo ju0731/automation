e8aaa15c0c7932e04d30f02d5c0f8dc2f5116371
ju0731 <60119657+ju0731@users.noreply.github.com>
2020-05-06 16:47:53 +0900

    Update applicationRun.sh

--- applicationRun.sh
+++ applicationRun.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/front
 
-timestamp=`date +%Y%m%d%H%M`
-echo "$timestamp" > time1.json
+echo "test" > test.json
 
 sleep 40s
 npm run serve
-echo "$timestamp" > time2.json
