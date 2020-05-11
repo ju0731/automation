fcb92553d62d0af250983c0dbe0d4b2260df86ab
root <root@ip-172-31-21-6.ap-northeast-2.compute.internal>
2020-05-11 06:05:00 +0000

    daemon

--- applicationRun.sh
+++ applicationRun.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cd /home/ec2-user/front
-
-npm run serve
+sudo mv /home/ec2-user/front/front.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl start front.service
+sudo systemctl enable front.service
+sudo systemctl restart front.service
